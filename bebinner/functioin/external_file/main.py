5da713fc3683d578dacd042a89907f18db6ae904
YumikoMiyauchi <31341272+YumikoMiyauchi@users.noreply.github.com>
2017-08-26 20:25:38 +0900

    Update main.py

--- bebinner/functioin/external_file/main.py
+++ bebinner/functioin/external_file/main.py
@@ -12,3 +12,20 @@ def main():
     check_even_odd(25)
     check_even_odd(33)
 
+
+    解説
+
+わざわざ、 if __name__ == '__main__': の下に main() のみ記述し、関数 main を定義して処理を記述しています。
+このような書き方を良くします。
+
+def 関数():
+    処理
+
+def main():
+    処理
+
+if __name__ == '__main__':
+    main()
+
+後ほどテストの解説で触れますが、 if __name__ == '__main__': の下に色々な処理を書くとテストが書きにくいです。
+そこで、 if __name__ == '__main__': の下には関数 main の実行だけ書きます。
