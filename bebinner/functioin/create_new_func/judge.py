d80f0afbfb258c07a067b7aaf45ada8532fefe6c
YumikoMiyauchi <31341272+YumikoMiyauchi@users.noreply.github.com>
2017-08-26 20:12:32 +0900

    Create judge.py

---
+++ bebinner/functioin/create_new_func/judge.py
@@ -0,0 +1,12 @@
+# 関数check_even_oddを定義します
+def check_even_odd(num):
+    if num%2 == 0:
+        print(num, 'is even number!')
+    else:
+        print(num, 'is odd number!')
+
+
+# 実行
+check_even_odd(14)
+check_even_odd(25)
+check_even_odd(33)
